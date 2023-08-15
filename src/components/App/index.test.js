41b5faa4e9379e4a21a410a608ec63be43f3459c
joeyzhang <luqi.zhang@hotmail.com>
2023-08-16 00:13:36 +1000

    unit test

--- src/components/App/index.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-
-import App from './index'
-
-test('render the home page', () => {
-  render(<App />)
-
-  expect(screen.getByText('Welcome to My Website')).toBeInTheDocument()
-})
